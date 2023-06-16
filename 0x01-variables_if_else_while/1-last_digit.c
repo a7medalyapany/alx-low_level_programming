695f63356baa841de8b253902edee48d22f66f8c
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-06-16 16:08:08 +0000

    up

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: ALX tasks
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int ld = n % 10;
+
+	if (ld < 6 && != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+	}
+	else if (ld > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0", n, ld);
+	}
+	return (0);
+}
