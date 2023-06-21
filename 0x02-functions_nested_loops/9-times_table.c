922032ebda9392c0a6f2ce1b7a6dc1a7797aeac2
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-06-21 00:43:53 +0000

    modified

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,10 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			printf("%d, ", (i * j));
+			if (j !=9)
+				printf("%d, ", (i * j));
+			else
+				printf("%d$", (i * j));
 		}
 		printf("\n");
 	}
