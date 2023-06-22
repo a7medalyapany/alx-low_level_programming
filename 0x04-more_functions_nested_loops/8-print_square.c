57083fd5a053849771057774431e8d06502c4ad4
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-06-22 23:27:45 +0000

    msg

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,9 +11,7 @@ void print_square(int size)
 	int i, j;
 
 	if (size <= 0)
-	{
-		_putchar('\n');		
-	}
+		_putchar('\n');
 	else
 	{
 		for (i = 0; i < size; i++)
@@ -21,6 +19,6 @@ void print_square(int size)
 			for (j = 0; j < size; j++)
 				_putchar('#');
 			_putchar('\n');
-		}	
+		}
 	}
 }
