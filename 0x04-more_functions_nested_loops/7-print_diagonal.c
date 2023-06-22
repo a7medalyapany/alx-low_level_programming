e2aead79dd15a29dfe71e4deb554928b0f8b5615
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-06-22 22:34:58 +0000

    msg

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_duagonal - method for ALX task
+ * @n: ID for alx task
+ *
+ * Return: 1 if on success, 0 if not
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; j++)
+	{
+		for (j =0; j < n; j++)
+			_putchar(' ');
+
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
