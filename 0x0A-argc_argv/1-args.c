25851ce03749ba59eec838c2047feb8aa9b5ee9c
a7medalyapany <103336732+a7medalyapany@users.noreply.github.com>
2023-07-08 01:35:40 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,8 +9,11 @@
  */
 int main(int argc, char *argv[])
 {
-	int count = argc - 1;
+	int count = 0;
+	
+	while (argv[count])
+		count++;
 
-	printf("%d\n", count);
+	printf("%d\n", count - 1);
 	return (0);
 }
