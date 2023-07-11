74743125ab962678b05ad396d596e93083dab968
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-07-11 17:00:21 +0000

    intial

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to a newly allocated space in memory,
+ *           which contains a duplicate of the string str.
+ * @str: The string to be duplicated.
+ *
+ * Return: A pointer to the duplicated string, or NULL if str is NULL
+ *         or if insufficient memory was available.
+ */
+char *_strdup(char *str)
+{
+	char *duplicate;
+	unsigned int length, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	length = 0;
+	while (str[length] != '\0')
+		length++;
+
+	duplicate = malloc(sizeof(char) * (length + 1));
+	if (duplicate == NULL)
+		return (NULL);
+
+	for (i = 0; i <= length; i++)
+		duplicate[i] = str[i];
+
+	return (duplicate);
+}
+
---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,53 @@
+#include <main.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - Concatenates all the arguments of a program.
+ * @ac: The number of arguments.
+ * @av: The array of arguments.
+ *
+ * Return: A pointer to the concatenated string, or NULL.
+ */
+char *argstostr(int ac, char **av)
+{
+	char *concatenated;
+	int i, j, k, total_length;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	total_length = 0;
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			total_length++;
+			j++;
+		}
+		total_length++;
+	}
+
+	concatenated = malloc(sizeof(char) * (total_length + 1));
+	if (concatenated == NULL)
+		return (NULL);
+
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			concatenated[k] = av[i][j];
+			k++;
+			j++;
+		}
+		concatenated[k] = '\n';
+		k++;
+	}
+
+	concatenated[k] = '\0';
+
+	return (concatenated);
+}
+
