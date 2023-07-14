4ada33f9d8e0d94d51cb80a19af6f4ae195e379b
a7medalyapany <103336732+a7medalyapany@users.noreply.github.com>
2023-07-14 03:15:32 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,14 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <string.h>
 
-int multiply_numbers(char *num1, char *num2);
-int is_positive_number(char *num);
+void multiply(char *num1, char *num2);
+int is_positive_number(const char *num);
 void print_error_and_exit(void);
-int string_to_int(char *str);
+void reverse_string(char *str);
+void add_strings(char *result, char *num);
+char *allocate_memory(int size);
 
 int main(int argc, char *argv[])
 {
@@ -15,27 +18,51 @@ int main(int argc, char *argv[])
 	if (!is_positive_number(argv[1]) || !is_positive_number(argv[2]))
 		print_error_and_exit();
 
-	int result = multiply_numbers(argv[1], argv[2]);
-	printf("%d\n", result);
+	multiply(argv[1], argv[2]);
 
 	return (0);
 }
 
-int multiply_numbers(char *num1, char *num2)
+void multiply(char *num1, char *num2)
 {
-	int n1 = string_to_int(num1);
-	int n2 = string_to_int(num2);
+	int len1 = strlen(num1);
+	int len2 = strlen(num2);
+	int total_length = len1 + len2;
+	char *result = allocate_memory(total_length + 1);
+	int carry = 0;
 
-	return (n1 * n2);
+	reverse_string(num1);
+	reverse_string(num2);
+
+	for (int i = 0; i < len1; i++)
+	{
+		carry = 0;
+
+		for (int j = 0; j < len2; j++)
+		{
+			int digit1 = num1[i] - '0';
+			int digit2 = num2[j] - '0';
+
+			int temp = (result[i + j] - '0') + (digit1 * digit2) + carry;
+			result[i + j] = (temp % 10) + '0';
+			carry = temp / 10;
+		}
+
+		result[i + len2] += carry;
+	}
+
+	reverse_string(result);
+	printf("%s\n", result);
+
+	free(result);
 }
 
-int is_positive_number(char *num)
+int is_positive_number(const char *num)
 {
-	while (*num)
+	for (int i = 0; num[i] != '\0'; i++)
 	{
-		if (!isdigit(*num))
+		if (num[i] < '0' || num[i] > '9')
 			return (0);
-		num++;
 	}
 
 	return (1);
@@ -47,13 +74,49 @@ void print_error_and_exit(void)
 	exit(98);
 }
 
-int string_to_int(char *str)
+void reverse_string(char *str)
 {
-	int result = 0;
-	while (*str)
+	int i = 0;
+	int j = strlen(str) - 1;
+
+	while (i < j)
 	{
-		result = (result * 10) + (*str - '0');
-		str++;
+		char temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+		i++;
+		j--;
 	}
-	return (result);
+}
+
+void add_strings(char *result, char *num)
+{
+	int carry = 0;
+	int len = strlen(num);
+
+	for (int i = 0; i < len; i++)
+	{
+		int digit = result[i] - '0' + num[i] - '0' + carry;
+		result[i] = (digit % 10) + '0';
+		carry = digit / 10;
+	}
+
+	while (carry)
+	{
+		result[len] = (carry % 10) + '0';
+		carry /= 10;
+		len++;
+	}
+
+	result[len] = '\0';
+}
+
+char *allocate_memory(int size)
+{
+	char *ptr = malloc(size * sizeof(char));
+	if (ptr == NULL)
+	{
+		print_error_and_exit();
+	}
+	return (ptr);
 }
