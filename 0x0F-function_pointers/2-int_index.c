8b6ad6cf8badea7e0d2bc31346d3e90e55854675
a7medalyapany <103336732+a7medalyapany@users.noreply.github.com>
2023-07-20 05:12:10 +0300

    Create 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * int_index - searches for an integer in an array
+ * @array: pointer to the array
+ * @size: number of elements in the array
+ * @cmp: pointer to the function used to compare values
+ *
+ * Return: index of the first element for which cmp function does not return 0
+ *         -1 if no element matches or if size is <= 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+    if (array && cmp)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (cmp(array[i]))
+                return i;
+        }
+    }
+    return -1;
+}
+
+/**
+ * is_98 - check if a number is equal to 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int is_98(int elem)
+{
+    return (98 == elem);
+}
+
+/**
+ * is_strictly_positive - check if a number is greater than 0
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int is_strictly_positive(int elem)
+{
+    return (elem > 0);
+}
+
+
+/**
+ * abs_is_98 - check if the absolute value of a number is 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int abs_is_98(int elem)
+{
+    return (elem == 98 || -elem == 98);
+}
