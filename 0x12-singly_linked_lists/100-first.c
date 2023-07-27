3f1309af8abd3ec78d3102f4bba3c68342ddfa0e
a7medalyapany <103336732+a7medalyapany@users.noreply.github.com>
2023-07-27 20:55:59 +0300

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void print_message(void) __attribute__((constructor));
+
+void print_message(void)
+{
+  printf("You're beat! and yet, you must allow,\n"
+           "I bore my house upon my back!\n");
+}
