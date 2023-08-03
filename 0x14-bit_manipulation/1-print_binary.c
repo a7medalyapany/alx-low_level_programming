2b7466ee78daccbdc822db67c46bd29773ba1e45
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-08-03 21:19:37 +0000

    initial

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,19 +9,24 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int printed = 0;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	while (mask > 0)
+	while (mask)
 	{
 		if (n & mask)
+		{
 			_putchar('1');
-		else
+			printed = 1;
+		}
+		else if (printed)
+		{
 			_putchar('0');
+		}
+
 		mask >>= 1;
 	}
+
+	if (!printed)
+		_putchar('0');
 }
+
