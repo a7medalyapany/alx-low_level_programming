ff4323bf0187f7c49b6318c7fe714d80f63873fb
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-08-08 00:55:54 +0000

    initial

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!buffer)
 		return (0);
 
-	fileDescriptor = open(filename, 0_RDONLY);
+	fileDescriptor = open(filename, O_RDONLY);
 	if (fileDescriptor == -1)
 	{
 		free(buffer);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
