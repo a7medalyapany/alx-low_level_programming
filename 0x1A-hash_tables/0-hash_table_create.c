80f66f96b41626354e5ffdc731261f7da24a8e0d
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-10-05 18:29:17 +0300

    initial

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,9 +8,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *new_table;
+	unsigned long int i;
 
-	ht = hash_table_create(1024);
-	printf("%p\n", (void *)ht);
-	return (EXIT_SUCCESS);
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+
+	return (new_table);
 }
