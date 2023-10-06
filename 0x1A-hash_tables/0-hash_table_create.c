67bbf89b8551c236d6b1d2bfe2911c9f54ba5a14
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2023-10-06 05:49:34 +0300

    initial

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -24,7 +24,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	for (i = 0; i < size; i++)
 	{
-		hash_table->array[i] = NULL;
+		new_table->array[i] = NULL;
 	}
 
 	return (new_table);
